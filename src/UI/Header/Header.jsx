45273879fd7259034e9a8819e7bfce84e12580d3
RaphRastelli <raphael.rastelli@gmail.com>
2026-03-27 16:10:08 +0100

    Initial

---
+++ src/UI/Header/Header.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router';
+
+export default function Header(){
+  return(
+    <header className={style['headstyle']}>
+      <h1>
+        <Link to="/">MusicianFinder</Link>
+      </h1>
+    </header>
+  )
+}
